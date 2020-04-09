a4956549824050e908099a89521367c1d0075858
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2020-04-09 19:29:13 +0800

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -3,7 +3,7 @@ import time
 import re
 from slackclient import SlackClient
 
-SLACK_BOT_TOKEN = 'xoxb-1054091500085-1070223311200-WfywkMOuEqN1g6a5IG4sqwdl'
+SLACK_BOT_TOKEN = 'xoxb-1054091500085-1070223311200-mYOQpEN4ni1ENBruDR7lMy60'
 
 # instantiate Slack client
 slack_client = SlackClient(SLACK_BOT_TOKEN)
